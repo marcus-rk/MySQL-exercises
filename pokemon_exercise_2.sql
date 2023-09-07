a40c6136e02f64455b881b0b829e26176b51e28d
marcus-rk <88247953+marcus-rk@users.noreply.github.com>
2023-09-07 22:46:11 +0200

    Update pokemon_exercise_2.sql and added Q5 and some homemade questions

--- pokemon_exercise_2.sql
+++ pokemon_exercise_2.sql
@@ -12,6 +12,20 @@ SELECT COUNT(*) AS total_pokemon
 FROM pokemon;
 
 /* Q3: What is the maximum primary type? (A: Water) */
+SELECT
+	MAX(primary_type)
+FROM pokemon;
+
+/* Q4: What is the minimum primary type? (A: Bug) */
+SELECT
+	MIN(primary_type)
+FROM pokemon;
+
+/* Q5: Why is 'Water' the maximum and 'Bug' the minimum? Make an informed guess 
+I think it's because the primary_type 'Water' has the most characters and 'Bug' has the least characters
+*/
+
+/* Homemade question: What primary_type has the biggest count? (A: Water) */
 SELECT 
 	primary_type, 
 	COUNT(primary_type) AS total
@@ -20,9 +34,7 @@ GROUP BY primary_type
 ORDER BY total DESC;
 -- LIMIT 1;
 
-/* Q4: What is the minimum primary type? (A: Bug) 
-Should be 'Fairy' or 'Ice' if u ask me :)
- */
+/* Homemade question: What primary_type has the lowest count? (A: Fairy or Ice) */
 SELECT 
 	primary_type, 
 	COUNT(primary_type) AS total
@@ -31,9 +43,7 @@ GROUP BY primary_type
 ORDER BY total ASC;
 -- LIMIT 1;
 
-/* Q5: Why is 'Water' the maximum and 'Bug' the minimum? Make an informed guess */
-
-/* Q6: What is the max speed of all pokemon? (A: 140) */
+/* Q6: What is the max speed of all pokemons? (A: 140) */
 SELECT MAX(speed) AS max_speed
 FROM pokemon;
           -- Could also be done like this:
@@ -45,14 +55,14 @@ LIMIT 1;
 /* Q7: What is the average speed of all pokemon with 'Ground' as primary type? (A: 58.1250) */
 SELECT
 	primary_type,
-    AVG(speed) AS average_speed
+	AVG(speed) AS average_speed
 FROM pokemon
 WHERE primary_type = "Ground";
 
-/* Q8: Display the average speed of each primary type of pokémon */
+/* Q8: Display the average speed of each primary type of pokemon */
 SELECT
 	primary_type,
-    AVG(speed) AS average_speed
+	AVG(speed) AS average_speed
 FROM pokemon
 GROUP BY primary_type
 ORDER BY average_speed DESC;
